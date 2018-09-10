21fb9b661cf89de9b0cc68e66cb61802a204c27b
Joe Zhou <zhouyangyu@hotmail.com>
2018-09-10 18:16:59 -0400

    🔧  minor change, 💎  0.3.3 -> 0.3.4

--- click_mergevcfs/commands.py
+++ click_mergevcfs/commands.py
@@ -130,7 +130,7 @@ def caveman_postprocess(perl_path, flag_script, in_vcf, out_vcf, normal_bam,
             '-g', indelBed,
             '-umv', unmatchedVCFLoc,
             '-ref', reference + ".fai", # Reference index (fai) from caveman help
-            '-t', 'pulldown' if sequencing_method == 'TGD' else 'genome',
+            '-t', 'pulldown' if sequencing_method == 'TGD' else 'genomic',
             '-c', flagConfig,
             '-v', flagToVcfConfig,
             '-ab', annoBedLoc,
